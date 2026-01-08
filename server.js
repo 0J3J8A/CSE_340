72a013507ade7a6cc43442739ecf94ffa580a320
0J3J8A <juanjoaguirre38@gmail.com>
2026-01-07 21:00:25 -0500

    adding css

--- server.js
+++ server.js
@@ -9,31 +9,33 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
+
+/* ***********************
+ * Middleware
+ *************************/
+app.use(express.static("public"))  // <-- AÑADE ESTA LÍNEA
+app.use(expressLayouts)
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.set("layout", "./layouts/layout")
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
-
 // Index Route
 app.get("/", function(req, res) {
   res.render("index", { title: "Home" })
 })
 
 /* ***********************
- * View Engine and Templates
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
-
-/* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 3000
+const host = process.env.HOST || "localhost"
 
 /* ***********************
  * Log statement to confirm server operation
